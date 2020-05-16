838cbb575a27f76c93c8167a4386f34c03329981
unknown <prabinshrestha737@gmail.com>
2020-05-16 16:14:34 +0545

    done

---
+++ index.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+
+<div class="topnav">
+		<img src="logo1.png" style="height: 6%;width: 6%";>
+  <div class="topnav-right">
+    <a href="#Home">Home</a>
+    <a href="#Shop">Shop</a>
+    <a href="#services">Our services</a>
+    <a href="#Contact">Contact us</a>
+  </div>
+</div><br>
+
+
+<div style="margin-left: 20%;margin-right: 20%;">
+	<img src="test.jpg" style="height: 100%; width: 100%;">
+
+</div><br><br>
+<div style="margin-left: 20%;margin-right: 20%;">
+Bakery businesses, together with tortilla manufacturing companies, employed about 18 percent of food manufacturing industry employees in 2008, according to the Bureau of Labor Statistics in the online report, “Food Manufacturing.” The United States commercial bakery industry generates about $28 billion in revenues, with the sector’s top 50 companies earning 80 percent of that income, according to Hoover.com in an online report summary entitled, “Bakery Products.”<br>
+
+<h3> History: </h3>
+
+Baked goods extend back to ancient history, according to Stephen Holloway, writing for Kitchen Project.com in the online article, “The History of Bread.” Egyptians baked bread thousands of years ago, and bakers joined bakers’ guilds in Ancient Rome. In the United States, colonial-era bakeries were social places where people gathered to warm up and gossip, according to Food Timeline.org in the online article, “Colonial and Early American Fare.”<br>
+
+<h3>Types</h3>
+Bakeries can range from small artisan businesses, specializing in one or two items, to large industrial bakeries that supply food retailers with packaged baked goods, according to A Touch of Business.com in the online article, “How to Start a Bakery.” Franchise bakeries create baked goods with access to the brand recognition, customer base and mentorship opportunities available after paying regular franchise fees. Bakeries may also venture into catering opportunities; for example, delivering hot muffins and donuts and fresh orange juice to business offices before important meetings. Bakeries sometimes specialize in just one or two items, such as wedding cakes or bagels prepared according to kosher standards. You can also start a bakery specializing in baked goods for pets, according to Style Career in the online article, “Pet Bakery.”
+
+
+</div>
+</body>
+</html>
